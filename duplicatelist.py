9ff434e43557dfc73bc94df11d1bba2ee27b8ecf
pendemjayasri98 <86804885+pendemjayasri98@users.noreply.github.com>
2024-12-21 16:39:40 +0530

    modiefied changes

--- duplicatelist.py
+++ duplicatelist.py
@@ -1,13 +1,15 @@
 L = [1, 2, 2, 2, 4, 3, 4]
-duplicate = []
-d={}
+# duplicate = []
+dic={}
 for i in range(0, len(L)):
+    value = L[i]
     for j in range(i + 1, len(L)):
-        if L[j] == L[i]:
-            duplicate.append(L[i])
+        if L[j] == value:
+            dic[value] = value
 
+print(dic.keys())
 
-print(duplicate)
+#-------------------------------------
 
 L = [1, 2, 2, 2, 4, 3, 4]
 d=[]
